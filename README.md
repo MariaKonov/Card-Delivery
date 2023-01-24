4394593a555b553495cbc661e46bc4274a2e21cc
Maria <marijka000@gmail.com>
2023-01-24 19:00:58 +0300

    New Add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Card-Delivery
+
+[![Build status](https://ci.appveyor.com/api/projects/status/fqcn0811fs9qkrw8?svg=true)](https://ci.appveyor.com/project/MariaKonov/card-delivery)
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,34 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
 public class CardTest {
+
+    private String Date (int addDays, String patten) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(patten));
+    }
+
+    @Test
+    void CardDeliveryTest() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Волгоград");
+        String currentDate = Date(3, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        $x("//input [@name = 'name']").setValue("Коновалова Мария");
+        $x("//input [@name ='phone']").setValue("+79996665544");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactOwnText("Встреча успешно забронирована на " + currentDate));
+    }
 }
